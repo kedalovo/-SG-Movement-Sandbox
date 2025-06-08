78e45cfd1458d00abfb769d0e4e451d2870da8ce
kedalovo <69755184+kedalovo@users.noreply.github.com>
2025-06-08 19:29:57 +0400

    Finished moving cubes. Changed platform exported variables handling for more complex behaviour. Added light emission to gravity areas' fog and particles.

--- Platform/platform.gd
+++ Platform/platform.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 @onready var pivot: Node3D = $Pivot
 @onready var body: AnimatableBody3D = $Body
-@onready var mesh: CSGBox3D = $Body/Mesh
+@onready var mesh: CSGBox3D = $Body/Shape/Mesh
 @onready var shape: CollisionShape3D = $Body/Shape
 
 @onready var destination: Node3D = $Destination
@@ -12,7 +12,7 @@ extends Node3D
 
 var _size: Vector3 = Vector3.ONE
 var _destination_pos: Vector3 = Vector3.ZERO
-var _time: float = 0.0
+var _time: float = 1.0
 var _rot_speed: Vector3 = Vector3.ZERO
 var _wait_time: float = 0.0
 
@@ -24,11 +24,12 @@ var _wait_time: float = 0.0
 		_size = v
 		size = v
 		if Engine.is_editor_hint():
-			mesh = $Body/Mesh
-			shape = $Body/Shape
-			shape.shape = BoxShape3D.new()
-			mesh.size = v
-			shape.shape.size = v
+			body = $Body
+			for s in body.get_children():
+				s.shape = BoxShape3D.new()
+				s.shape.size = v
+				var m: CSGBox3D = s.get_child(0)
+				m.size = v
 
 @export var destination_pos: Vector3 = Vector3.ZERO:
 	get:
@@ -40,7 +41,7 @@ var _wait_time: float = 0.0
 			destination = $Destination
 			destination.position = _destination_pos
 
-@export var time: float = 0.0:
+@export var time: float = 1.0:
 	get:
 		return _time
 	set(v):
@@ -61,25 +62,33 @@ var _wait_time: float = 0.0
 		_wait_time = clampf(v, 0.0, 30.0)
 		wait_time = v
 
+@export var ease_type: Tween.EaseType = Tween.EASE_IN_OUT
+@export var trans_type: Tween.TransitionType = Tween.TRANS_QUART
+
 var is_going_to: bool = true
 
 
 func _ready() -> void:
 	set_physics_process(false)
-	shape.shape = BoxShape3D.new()
+	for s in body.get_children():
+		s.shape = BoxShape3D.new()
+		s.shape.size = size
+		var m: CSGBox3D = s.get_child(0)
+		m.size = size
 	if !Engine.is_editor_hint():
-		shape.shape = BoxShape3D.new()
 		if rot_speed != Vector3.ZERO:
 			set_physics_process(true)
 		else:
 			body.constant_angular_velocity = rot_speed
-		mesh.size = size
-		shape.shape.size = size
+		#for s in body.get_children():
+			#s.size = size
+			#var m: CSGBox3D = s.get_child(0)
+			#m.size = size
 		destination.position = _destination_pos
 		destination.hide()
-		if destination_pos != Vector3.ZERO and time > 0.0:
+		if destination_pos != Vector3.ZERO:
 			var pos_tween := get_tree().create_tween()
-			pos_tween.tween_property(body, "global_position", destination.global_position, time)
+			pos_tween.tween_property(body, "global_position", destination.global_position, time).set_ease(ease_type).set_trans(trans_type)
 			if wait_time > 0.0:
 				pos_tween.finished.connect(func(): wait_timer.start(wait_time))
 			else:
@@ -96,7 +105,7 @@ func _on_wait_timer_timeout() -> void:
 	if is_going_to:
 		is_going_to = false
 		var pos_tween := get_tree().create_tween()
-		pos_tween.tween_property(body, "global_position", global_position, time).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
+		pos_tween.tween_property(body, "global_position", global_position, time).set_ease(ease_type).set_trans(trans_type)
 		if wait_time > 0.0:
 			pos_tween.finished.connect(func(): wait_timer.start(wait_time))
 		else:
@@ -104,7 +113,7 @@ func _on_wait_timer_timeout() -> void:
 	else:
 		is_going_to = true
 		var pos_tween := get_tree().create_tween()
-		pos_tween.tween_property(body, "global_position", destination.global_position, time).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
+		pos_tween.tween_property(body, "global_position", destination.global_position, time).set_ease(ease_type).set_trans(trans_type)
 		if wait_time > 0.0:
 			pos_tween.finished.connect(func(): wait_timer.start(wait_time))
 		else:
